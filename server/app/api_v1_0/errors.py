98e209f30bfa126cf0956d1e0e5197ae3cd17c2d
BeanWei <157318439@qq.com>
2018-04-14 23:03:42 +0800

    :sparkles:增加无权限的返回值

--- server/app/api_v1_0/errors.py
+++ server/app/api_v1_0/errors.py
@@ -4,5 +4,9 @@ class Error:
         return {'error': 'page not found'}
 
     @property
+    def forbidden(self):
+        return {'error': '403 forbidden'}
+
+    @property
     def internal_server_error(Self):
         return {'error': 'internal server error'}
